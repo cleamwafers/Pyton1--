96db35a4c5c24266853c4470076c300cd58c3c0a
cleamwafers <cleamwafers@yahoo.co.jp>
2023-05-12 19:32:05 +0900

    カメでお絵描きしてみよう！

---
+++ turtle1.py
@@ -0,0 +1,17 @@
+#IDLEにて実行する
+from turtle import*
+#1行ずつ入力する
+shape("turtle")
+#1行ずつ入力する
+forward(100)
+#1行ずつ入力する
+forward(200)
+#1行ずつ入力する
+back(300)
+#1行ずつ入力する
+left(90)
+#1行ずつ入力する
+right(90)
+#1行ずつ入力する
+done()
+#1行ずつ入力する
