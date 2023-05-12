23b5bed0cb418bbace70a86f157c93487f9a070d
cleamwafers <cleamwafers@yahoo.co.jp>
2023-05-12 19:09:09 +0900

    input機能(BMI値計算プログラム)

--- bmi.py
+++ bmi.py
@@ -1,4 +1,4 @@
-h = float(input("身長何cmですか？"))/100.0
-w = float(input("体重何kgですか？"))
+h = float(input("171"))/100.0
+w = float(input("64"))
 bmi = w / (h*h)
-print("あなたのBMI値は、"bmi,"です。")
+print("あなたのBMI値は、",bmi,"です。")
