68c040c97957e02cfc2e4a675788250a0cb95090
cleamwafers <cleamwafers@yahoo.co.jp>
2023-05-12 21:56:00 +0900

    カラフルな花を描く

---
+++ turtle4.py
@@ -0,0 +1,18 @@
+#IDLEにて実行する
+from turtle import*
+#1行ずつ入力する
+shape("turtle")
+#1行ずつ入力する
+col =["orange","limegreen","gold","plum","tomato"]
+#1行ずつ入力する
+for i in range(5):
+#1行ずつ入力する
+    color(col[i])
+    #1行ずつ入力する
+    circle(100)
+    #1行ずつ入力する
+    left(72)
+    #1行ずつ入力する  
+#Enterを入力する
+done()
+#1行ずつ入力する
