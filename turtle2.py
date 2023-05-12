f4e42f491d7352f2141b9d514a7a6d3ddb10a0c3
cleamwafers <cleamwafers@yahoo.co.jp>
2023-05-12 21:08:35 +0900

    正方形を描く

---
+++ turtle2.py
@@ -0,0 +1,13 @@
+from turtle import*
+#1行ずつ入力する
+shape("turtle")
+#1行ずつ入力する
+for _ in range(4):
+    #1行ずつ入力する
+    forward(100)
+    #1行ずつ入力する
+    left(90)
+    #1行ずつ入力する
+#Enterを入力する
+    done() 
+#1行ずつ入力する
