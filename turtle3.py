476b7df4f93d5dcebe15112cfe4049aeb8f18bba
cleamwafers <cleamwafers@yahoo.co.jp>
2023-05-12 21:30:52 +0900

    カラフルな星を描く

---
+++ turtle3.py
@@ -0,0 +1,17 @@
+from turtle import*
+ #1行ずつ入力する
+shape("turtle")
+ #1行ずつ入力する
+col = ["orange","limegreen","gold","plum","tomato"]
+ #1行ずつ入力する
+for i in range(5):
+     #1行ずつ入力する
+    color(col[i])
+     #1行ずつ入力する
+    forward(200)
+     #1行ずつ入力する
+    left(144)
+     #1行ずつ入力する
+#Enterを入力する
+done()
+#1行ずつ入力する
