04e1f6ea15e177c9ed931b65d54ff255b1a2773c
cleamwafers <cleamwafers@yahoo.co.jp>
2023-05-13 21:46:38 +0900

    モジュールを作ってみよう

--- import1.py
+++ import1.py
@@ -1,5 +1,5 @@
-import tax
+import tax1
 
-print(tax.postTaxPrice(120),"円")
-print(tax.postTaxPrice(128),"円")
-print(tax.postTaxPrice(980),"円")
+print(tax1.postTaxPrice(120),"円")
+print(tax1.postTaxPrice(128),"円")
+print(tax1.postTaxPrice(980),"円")
