7a8d1dfc2834f1c3ae35cff5bb961114e4e0c3fa
cleamwafers <cleamwafers@yahoo.co.jp>
2023-05-14 12:24:17 +0900

    LESSON19_fin

--- chapter04/displmage.py
+++ chapter04/displmage.py
@@ -19,9 +19,14 @@ def dispPhoto(path):
 def openFile():
     #ファイルダイアログを開くための関数
     fpath = fd.askopenfilename()
-    #ファイルダイアログを開いて、選択したファイル名を取得する
+    """
+    ファイルダイアログを開いて、選択したファイル名を取得する(ファイルダイアログが出て居る間はここの行で止まった状態になる。)
+    OpenまたはCancelボタンをクリックすると、選んだファイル名がfpath変数に入ってきて、次の行へ進む。
+    Cancelボタンを選ぶと変数の中に空っぽな値として入ってくる。
+    """
+
     if fpath:
-    #もしファイル名があったら
+    #調べている。もしファイル名があったら・・・
         dispPhoto(fpath)
         #そのファイル名で関数を呼び出す
 root = tk.Tk()
@@ -29,12 +34,19 @@ root.geometry("400x350")
 #上記２つは画面を作る
 
 btn = tk.Button(text="ファイルを開く", command = openFile)
-#ボタンを作って関数を指定する
+#ボタンを作って関数を指定する(ボタンがクリックされたらファイルダイアログを開く関数を実行する。)
 imageLabel = tk.Label()
 #画面表示用のラベルを作る
 btn.pack()
-#画面にボタンを配置する
+#画面にボタンを配置する(btn→imageLabelの順でpack命令をしているのでボタン、画像ラベルの順で配置される。)
 imageLabel.pack()
 #画面にラベルを配置する
 tk.mainloop()
 #作ったウィンドウを表示する
+
+"""
+プログラムは上から順番に実行していく。
+関数は関数を呼び出すところより上に書く。
+例1:『commmand = openfile』を書くならopenFile関数はそれより上に。
+例2:openFile関数の中で『disPhoto(fpath)』を呼び出すなら、disPhoto関数はopenFile関数より上に書かなければいけない。
+"""
