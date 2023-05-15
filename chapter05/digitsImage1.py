4373721d3ba31f92cf8384f2a1a5ba49e6f6c430
cleamwafers <cleamwafers@yahoo.co.jp>
2023-05-15 21:12:12 +0900

    学習用データ50個を確認する

--- chapter05/digitsImage1.py
+++ chapter05/digitsImage1.py
@@ -1,7 +1,10 @@
 import sklearn.datasets
 import matplotlib.pyplot as plt
+#matplotlib=画像表示アプリ
 
 digits = sklearn.datasets.load_digits()
 
 plt.imshow(digits.images[0], cmap="Greys")
+#数値データをグレーの濃淡画像にする
 plt.show()
+#作った画像を表示する
