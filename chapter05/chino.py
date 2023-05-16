0827f38466c2de77e76ff0339d78f5da18f717d1
cleamwafers <cleamwafers@yahoo.co.jp>
2023-05-16 09:27:43 +0900

    人工知能アプリ『チノ』を完成させよう

--- chapter05/chino.py
+++ chapter05/chino.py
@@ -2,6 +2,10 @@ import tkinter as tk
 import tkinter.filedialog as fd
 import PIL.Image 
 import PIL.ImageTk
+# 機械学習で使うモジュール
+import sklearn.datasets
+import sklearn.svm
+import numpy
 
 # 画像ファイルを数値リストに変換する
 def imageToData(filename):
@@ -12,6 +16,22 @@ def imageToData(filename):
     dispImage = PIL.ImageTk.PhotoImage(grayImage.resize((300,300),resample=0))
     imageLabel.configure(image = dispImage)
     imageLabel.image = dispImage
+    # 数値リストに変換
+    numImage = numpy.asarray(grayImage, dtype = float)
+    numImage = numpy.floor(16 - 16 * (numImage / 256))
+    numImage = numImage.flatten()
+    return numImage
+
+# 数字を予測する
+def predictDigits(data):
+    # 学習用データを読み込む
+    digits = sklearn.datasets.load_digits()
+    # 機械学習する
+    clf = sklearn.svm.SVC(gamma = 0.001)
+    clf.fit(digits.data, digits.target)
+    # 予測結果を表示する
+    n = clf.predict([data])
+    textLabel.configure(text = "この画像は"+str(n)+"です！")
 
 # ファイルダイアログを開く
 def openFile():
@@ -19,6 +39,8 @@ def openFile():
     if fpath:
         # 画像ファイルを数値リストに変換する
         data = imageToData(fpath)
+        # 数字を予測する
+        predictDigits(data)
 
 # アプリのウィンドウを作る
 root = tk.Tk()
@@ -26,9 +48,11 @@ root.geometry("400x400")
 
 btn = tk.Button(root, text="ファイルを開く", command = openFile)
 imageLabel = tk.Label()
-
 btn.pack()
 imageLabel.pack()
 
-tk.mainloop()
+# 予測結果を表示するラベル
+textLabel = tk.Label(text="手書きの数字を認識します！") 
+textLabel.pack()
 
+tk.mainloop()
